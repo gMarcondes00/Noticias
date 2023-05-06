8a23d94e0f17e7bec89a78f292635fcbb771b53c
gMarcondes00 <g.os.marcondes@gmail.com>
2023-05-06 19:28:06 -0300

    mudança da API

--- noticias/src/App.js
+++ noticias/src/App.js
@@ -8,14 +8,16 @@ import {useState, useEffect} from 'react';
 library.add();
 function App() {
   const [news, setNews] = useState(null);
-  const [search, setSearch] = useState("sao%paulo");
+  const [search, setSearch] = useState("");
   const [lang, setLang] = useState('pt');
   const [input, setInput] = useState('');
+  
+  const key = "26df8101fda71a66aa8a3f2f3be64f81"
 
-  const urlAPI = `https://newsapi.org/v2/everything?q=${search}&language=${lang}&apiKey=b9207aaccc944677b18cc7d1b7638a79`;
+  const urlAPI = `https://gnews.io/api/v4/search?q=${search}&lang=${lang}&apikey=${key}`;
 
   useEffect(()=>{
-    axios.get(urlAPI)
+   axios.get(urlAPI)
       .then(res=> setNews(res.data))
       .catch(err => console.log(err))
   },[urlAPI])
@@ -37,7 +39,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <h1>Carregando...</h1>
+        <h1>Insira uma palavra e receba notícias diversas sobre o assunto!</h1>
+          <input type='text' onChange={(e)=>setInput(e.target.value)} onKeyUp={handleKeyPress}/>
+          <button onClick={getNews}className='btn'><FontAwesomeIcon icon={faMagnifyingGlass} className='search'/></button>
+          <select name='select' class="btn btn-light" onChange={e=>setLang(e.target.value)}>
+            <option value="pt" selected>PT</option>
+            <option value="en">EN</option>
+            <option value="ru">RU</option>
+            <option value="fr">FR</option>
+            <option value="es">ES</option>
+            </select>
       </header>
     </div>
   );
@@ -48,7 +59,7 @@ function App() {
             <h1>Insira uma palavra e receba notícias diversas sobre o assunto!</h1>
             <input type='text' onChange={(e)=>setInput(e.target.value)} onKeyUp={handleKeyPress}/>
             <button onClick={getNews}className='btn'><FontAwesomeIcon icon={faMagnifyingGlass} className='search'/></button>
-            <select name='select' onChange={e=>setLang(e.target.value)}>
+            <select name='select' class="btn btn-light" onChange={e=>setLang(e.target.value)}>
               <option value="pt" selected>PT</option>
               <option value="en">EN</option>
               <option value="ru">RU</option>
@@ -58,10 +69,12 @@ function App() {
           <div className="noticia-wrapper">
             {news.articles.map((artigo, index)=>(
             <div key={index} className='noticia'>
-              <h1>{artigo.title}</h1>
-              <h2>{artigo.description}</h2>
-              <img src={artigo.urlToImage} alt='Foto da noticia' className='noticia-img'/>
-              <p>Confira mais <a href={artigo.url} target='_blank' rel="noreferrer">aqui</a></p>
+              <img src={artigo.image} alt='Foto da noticia' className="card-img-top noticia-img"/>
+              <div className='card-body'>
+                <h1 className="card-title">{artigo.title}</h1>
+                <h2 className="card-subtitle mb-2">{artigo.description}</h2>
+                <p className="card-text">Confira mais <a href={artigo.url} target='_blank' rel="noreferrer">aqui</a></p>
+              </div>
             </div>
             ))}
           </div>
